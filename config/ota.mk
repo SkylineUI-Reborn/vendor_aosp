d65436e95f1c9bf9a815f3a3a30b47f2676fc86c
GXC 2356 <gxcdev2356@gmail.com>
2024-09-19 07:17:58 +0000

    [TMP] config: ota: Force updater to be built in any build_type

--- config/ota.mk
+++ config/ota.mk
@@ -5,4 +5,3 @@ ifeq ($(CUSTOM_BUILD_TYPE),OFFICIAL)
 
     PRODUCT_COPY_FILES += \
         vendor/aosp/prebuilt/common/etc/init/init.custom-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-updater.rc
-endif
