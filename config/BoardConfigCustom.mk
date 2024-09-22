aebaa24a000b0d67a754636da627a9cc3187601e
Hridaya Prajapati <hridaya@pixelos.net>
2024-09-22 17:42:49 +0200

    Revert "lineage: Opt-in to shipping full recovery image by default"
    This reverts commit 0d73175fd573ab46f70bac5cc61ce89377902d1a.
    
    Change-Id: Ic19cd26b3df940391500fcec7bf6d32b74f491aa

--- config/BoardConfigCustom.mk
+++ config/BoardConfigCustom.mk
@@ -1,9 +1,6 @@
 # SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 
-# Recovery
-BOARD_USES_FULL_RECOVERY_IMAGE ?= true
-
 include vendor/aosp/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
